d925451d2fdd053538e2689a4233f83286fdff45
oshaman <reg_forall@bigmir.net>
2018-03-23 17:01:37 +0200

    articles_all

--- application/views/__helpers/Mdates.php
+++ application/views/__helpers/Mdates.php
@@ -24,8 +24,7 @@ class Zend_View_Helper_Mdates extends Zend_View_Helper_Abstract
             $year = date("Y", strtotime($date));
             $month = $months[date("n", strtotime($date))];
 
-//            return $day . ' ' . $month . ' ' . $year;
-            return var_dump($date);
+            return $day . ' ' . $month . ' ' . $year;
         }
         return '';
     }
