64d08ed4c1a94b9296db846f090869e0e1bc527c
oshaman <reg_forall@bigmir.net>
2018-03-23 13:53:03 +0200

    one-article-v-0-8

---
+++ application/views/__helpers/Mdates.php
@@ -0,0 +1,28 @@
+<?php
+
+class Zend_View_Helper_Mdates extends Zend_View_Helper_Abstract
+{
+
+    public function mdates($date)
+    {
+        /*if($date)
+        {
+            $months = array (
+                1 => 'января',
+                2 => 'февраля',
+                3 => 'марта',
+                4 => 'апреля',
+                5 => 'мая',
+                6 => 'июня',
+                7 => 'июля',
+                8 => 'августа',
+                9 => 'сентября',
+                10 => 'октября',
+                11 => 'ноября',
+                12 => 'декабря');
+
+            $format = str_replace("%m",$months[(int)date('m',$timestamp)],$format);
+            $formatter='auto';
+        }*/
+    }
+}
