ada611424b670e27115a9624705bfaf155d69766
oshaman <reg_forall@bigmir.net>
2018-05-17 18:34:28 +0300

    public_17_05

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -438,7 +438,7 @@ jQuery(document).ready(function () {
             var options = {
                 url: '/catalog/autocomplete',
                 minLength: 2,
-                modal: ".search-result",
+                modal: ".search-list__item_",
                 modal_error_class: ".search_ren",
                 noimageSrc: 'noimage.jpg'
             };
