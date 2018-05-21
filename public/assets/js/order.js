532bdbe571f8b779861176898c7b2528c2a784ee
e-portal <e-portal@bigmir.net>
2018-05-21 15:15:12 +0300

    fesak-

--- public/assets/js/order.js
+++ public/assets/js/order.js
@@ -79,7 +79,7 @@ $(document).ready(function () {
                 $('<span class="red"> + ' + obj[prod_data].bonus + ' грн</span> на <a>Бонусный счет</a>').appendTo('.basket-bonus' + i);
             }
 
-            if (obj[prod_data].sertTitle !== undefined) {
+            // if (obj[prod_data].sertTitle !== undefined) {
             $('<div class="basket-section-service flexibal">\n' +
                 '                                      <div class="basket-prod flexibal">\n' +
                 '                                           <div class="basket-prod-img"></div>\n' +
@@ -99,7 +99,7 @@ $(document).ready(function () {
                 '                                       <div class=""></div>\n' +
                 '                                    </div>').appendTo('.basket-section' /*+ i*/);
 
-           }
+           // }
 
 
 
