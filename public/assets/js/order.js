ecf8baf5bb3710ee4ff303b8e34fec0bef839ec1
e-portal <e-portal@bigmir.net>
2018-05-21 17:32:48 +0300

    fesak

--- public/assets/js/order.js
+++ public/assets/js/order.js
@@ -128,7 +128,7 @@ $(document).ready(function () {
 
     }
     function numeral(){
-        doBasket(obj);
+        doObject()
         console.log(i);
         $('.numeral').html(i);
         window.localStorage.setItem('basket', JSON.stringify(basket));
