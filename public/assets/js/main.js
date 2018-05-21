1c700e245e896ef86fff1beaa1708db55c17e993
e-portal <e-portal@bigmir.net>
2018-05-21 19:49:58 +0300

    fesak-

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1631,7 +1631,7 @@ $(document).ready(function () {
     btn = form.find('input[type="submit"]');
 
     form.on('submit', function (event) {
-        validform();
+
         if (!btn.length > 0) {
             btn = form.find('input[type="text"]');
             form.find('.valid').addClass('empty_field');
