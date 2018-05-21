641411bb8953f483ea5b774f4600bc5531f414e1
e-portal <e-portal@bigmir.net>
2018-05-21 10:29:51 +0300

    fesak-

--- public/assets/js/order.js
+++ public/assets/js/order.js
@@ -85,7 +85,7 @@ $(document).ready(function () {
                 '                                           <div class="basket-prod-img"></div>\n' +
                 '                                           <div class="certificates">\n' +
                 '                            <form>\n' +
-                '                                <label class="servs">'+ obj[prod_data].sert_title +'\n' +
+                '                                <label class="servs">'+ obj[prod_data].serttitle +'\n' +
                 '                                    <input type="checkbox" checked="checked">\n' +
                 '                                    <span class="checkmark"></span>\n' +
                 '                                    <!--<em>940 грн</em>-->\n' +
