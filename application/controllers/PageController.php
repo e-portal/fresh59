2360835b77eb742635e8db2ae02f3ab69c798e07
oshaman <reg_forall@bigmir.net>
2018-05-22 17:38:21 +0300

    public_22_05

--- application/controllers/PageController.php
+++ application/controllers/PageController.php
@@ -7,6 +7,13 @@ class PageController extends Alcotec_Frontend_Controller_PageController {
             $this->_redirect('/page/address',array('code'=>301));
         }
 
+        if ($this->_getParam('pagename') == 'address') {
+            $this->view->js_int = '
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCKFztHzVA7_L75JrTwyrzhk2asYAWUL7I&callback=initMap"></script>
+            <script src="/assets/js/map.js"></script>
+            ';
+        }
+
         $this->view->body_class = $this->_getParam('pagename') ? $this->_getParam('pagename') : null;
 
         parent::indexAction();
