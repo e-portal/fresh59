123014cc22b20fb224d7a9607bec8c6d171478cb
e-portal <e-portal@bigmir.net>
2018-05-23 14:20:46 +0300

    fesak-

--- public/assets/js/order.js
+++ public/assets/js/order.js
@@ -251,7 +251,7 @@ $(document).ready(function () {
         $(this).parents('.basket-section-prod').find('.prod-price-total').html((startPrice) * numcount);
         // $(this).parents('.basket-section-prod').find('.prod-price-total').attr('data-value', startPrice * numcount);
         totalCart();
-        console.log(numcount);
+        console.log(startPrice);
 
 
     }
