77b0a1a7d3a887af1141103af605cb04ce255fee
e-portal <e-portal@bigmir.net>
2018-05-23 14:26:49 +0300

    fesak-

--- public/assets/js/order.js
+++ public/assets/js/order.js
@@ -265,13 +265,12 @@ $(document).ready(function () {
 
     function totalCart() {
         totalPrice = 0;
-        $('.prod-price-total').change(function () {
+        totalPrice = parseInt($(this).html());
             $('.prod-price-total').each(function () {
                 totalPrice += parseInt($(this).html());
-
             });
             console.log(parseInt($(this).html()));
-        })
+
 
 
         $('.count-total').html(totalPrice);
