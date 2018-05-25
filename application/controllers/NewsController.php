ea1127f8a0addffdb31aa17b1c4c43550411ecaf
oshaman <reg_forall@bigmir.net>
2018-05-25 15:26:13 +0300

    public_24_05

--- application/controllers/NewsController.php
+++ application/controllers/NewsController.php
@@ -1,5 +1,9 @@
 <?php
 
 class NewsController extends Alcotec_Frontend_Controller_NewsController {
-	
+    public function indexAction()
+    {
+        $this->view->wrap = 'wrap';
+        parent::indexAction();
+    }
 }
