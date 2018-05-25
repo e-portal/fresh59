0594e0af60cb2096c17393ec25eee3be513f322f
oshaman <reg_forall@bigmir.net>
2018-05-25 17:54:52 +0300

    item_25_05

--- public/assets/js/product.js
+++ public/assets/js/product.js
@@ -182,14 +182,13 @@ function getCats(e) {
 
     if (("undefined" !== typeof _this) && _this.length !== 0) {
 
-        /*$('div[class$=data-catid-]').each(function (_this) {
+        $('.cat-items').each(function (_this) {
             $(this).hide();
             if (this.hasClass('data-catid-' + _this)) {
                 this.show();
             }
-        });*/
+        });
 
-        console.log($('div[class$=data-catid-]'));
 
         // console.log($('.data-catid-' + _this));
 
