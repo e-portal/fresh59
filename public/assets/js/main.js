9a8b4cec53c82f8a5647874689feb4f87e617a8d
oshaman <reg_forall@bigmir.net>
2018-06-16 16:39:32 +0300

    public_16_06

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1767,7 +1767,7 @@ function topNewProductIndex(catId, type) {
             // console.log(data, 1);
             /*ТУТ_ВАШ_КОД (ниже просто пример)*/
 
-            for (var i = 0; i < data.length; i++) {
+            // for (var i = 0; i < data.length; i++) {
                 console.log(data, 1);
                 $('<div class="item-senn">\n' +
                     ' <a class="sench" href="">\n' +
@@ -1833,7 +1833,7 @@ function topNewProductIndex(catId, type) {
 
                 console.log($data.items[i].imgid)
 
-            }
+            // }
 
 
         }
