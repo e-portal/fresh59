08508eade1065bf63b87158c009278436be157b7
oshaman <reg_forall@bigmir.net>
2018-06-16 15:13:18 +0300

    public_16_06

--- public/assets/js/order.js
+++ public/assets/js/order.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
 
         for (var i = 0; i < a.length; i++) {
-            console.log(a[i].length);
+            console.log(a.eq(i));
             // console.log(a.find('.basket-bonus').clone().appendTo('.basket-section-prod.flexibal'));
             // console.log(a.find('.basket-bonus').eq(0).remove());
             // a.find('.prod-price-total').clone().appendTo('.basket-prod.flexibal')
