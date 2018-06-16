edd51f38c016025b91d21896fe201b0f47f0815b
oshaman <reg_forall@bigmir.net>
2018-06-16 15:21:14 +0300

    public_16_06

--- public/assets/js/order.js
+++ public/assets/js/order.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
             // console.log(a.find('.basket-bonus').clone().appendTo('.basket-section-prod.flexibal'));
             // console.log(a.find('.basket-bonus').eq(0).remove());
             // a.find('.prod-price-total').clone().appendTo('.basket-prod.flexibal')
-            a.eq(1).find('.prod-price-total').remove()
+            a.eq(i).find('.prod-price-total').clone().appendTo(a.eq(i).find('.basket-prod-text'))
         }
 
         // console.log(
