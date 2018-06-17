50757aa090fc2844f1e7f848470130dd67db7fe8
oshaman <reg_forall@bigmir.net>
2018-06-17 14:23:39 +0300

    public_16_06

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1776,9 +1776,9 @@ function topNewProductIndex(catId, type) {
             // console.log(a, 123);
             $('.senn-main.maii .senn-slik.slick .slick-list .slick-track .maii-item .item-senn').remove()
 
-            // kk = data.items.length / 6
+            kk = data.items.length / 7
             // console.log(kk, 0000);
-            for (var n = 0; n < data.items.length; n++) {
+            for (var n = 0; n < kk; n++) {
                 // a = $('.maii-item')
                 $('<div class="maii-item"></div>').appendTo('.senn-main.maii .senn-slik')
                 n++
