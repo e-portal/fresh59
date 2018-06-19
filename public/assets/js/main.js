ec7810f1239f95bc74426c723aa294ce128f3d69
e-portal <e-portal@bigmir.net>
2018-06-19 17:58:15 +0300

    fesak_15_06

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1950,7 +1950,7 @@ function topNewProductIndex(catId, type) {
                     ' </object> \n' +
                     '<div class="cash"> 11999 грн.</div> \n' +
                     '<div class="times"> \n' +
-                    '<div class="timer" data-year="'+date_mas[3]+'" data-month="'+date_mas[1]+'" data-days="'+date_mas[0]+'"></div> \n' +
+                    '<div class="timer" data-year="'+date_mas[2]+'" data-month="'+date_mas[1]+'" data-days="'+date_mas[0]+'"></div> \n' +
                     '<div class="right-times"> \n' +
                     '<object type="lol/wut"> \n' +
                     '<a class="open-in-popup add-to-cart bask acty" data-id="'+data.items[i].id+'" data-name="' + data.items[i].cat_onename + ' ' + data.items[i].brand + ' ' + data.items[i].name + '" data-img="/images/catalog/80990_s.png" data-price="11999" data-sale="10750" data-bonus="1500" data-gift-id="" data-gift-name="Чайник ELECTROLUX Electro чайник" data-gift-img="/images/catalog/93266_s.jpg">  \n' +
