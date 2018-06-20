d4c1e5f9e2117bcd8224bfb1a1f4619449060f40
e-portal <e-portal@bigmir.net>
2018-06-20 16:12:14 +0300

    fesak_15_06

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -429,8 +429,8 @@ jQuery(document).ready(function () {
                     $('.menu').addClass('fixed_menu')
                     $('body').removeClass('fixed_menu_')
                 } else if(home_page_  + 30 > $(this).scrollTop()){
-                    $('.menu').removeClass('fixed_menu')
-                    $('body').removeClass('fixed_menu_')
+                    // $('.menu').removeClass('fixed_menu')
+                    // $('body').removeClass('fixed_menu_')
                 }
             }
 
